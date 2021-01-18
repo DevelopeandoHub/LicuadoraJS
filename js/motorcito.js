c593a15c7688fdc9b2094eca3c0a79d468ffa637
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-18 17:26:51 -0300

    funcionalidad de sonido terminada

--- js/motorcito.js
+++ js/motorcito.js
@@ -1,12 +1,25 @@
 /* Funcionamiento de la Licuadora */
 var estadoLicuadora = "apagado";
+var sonidoLicuadora = document.getElementById("blender-sound");
 
-function hacerBrrBrr() {
+
+function controlarLicuadora() {
   if (estadoLicuadora == "apagado") {
     estadoLicuadora = "encendido";
+    hacerBrrBrr();
     document.getElementById("blender-button").style.backgroundColor = "green";
   } else {
     estadoLicuadora = "apagado";
+    hacerBrrBrr();
     document.getElementById("blender-button").style.backgroundColor = "red";
+  }
+}
+
+function hacerBrrBrr() {
+if(sonidoLicuadora.paused){
+    sonidoLicuadora.play();
+  } else {
+    sonidoLicuadora.pause();
+    sonidoLicuadora.currentTime = 0; /* Reinicia el audio al segundo 0 */
   }
 }
