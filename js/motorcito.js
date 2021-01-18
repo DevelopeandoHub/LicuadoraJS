867eb71125dffd4c1cdea3eed37672f450d0a285
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-18 18:02:37 -0300

    sonido de boton agregado

--- js/motorcito.js
+++ js/motorcito.js
@@ -1,24 +1,27 @@
 /* Funcionamiento de la Licuadora */
 var estadoLicuadora = "apagado";
-var sonidoLicuadora = document.getElementById("blender-sound");
+var sonidoLicuadora = document.getElementById( "blender-sound" );
+var botonLicuadora = document.getElementById( "blender-button-sound" );
 
 
 function controlarLicuadora() {
-  if (estadoLicuadora == "apagado") {
+  if ( estadoLicuadora == "apagado" ) {
     estadoLicuadora = "encendido";
     hacerBrrBrr();
-    document.getElementById("blender-button").style.backgroundColor = "green";
+    document.getElementById( "blender-button" ).style.backgroundColor = "green";
   } else {
     estadoLicuadora = "apagado";
     hacerBrrBrr();
-    document.getElementById("blender-button").style.backgroundColor = "red";
+    document.getElementById( "blender-button" ).style.backgroundColor = "red";
   }
 }
 
 function hacerBrrBrr() {
-if(sonidoLicuadora.paused){
+if( sonidoLicuadora.paused ){
+    botonLicuadora.play();
     sonidoLicuadora.play();
   } else {
+    botonLicuadora.play();
     sonidoLicuadora.pause();
     sonidoLicuadora.currentTime = 0; /* Reinicia el audio al segundo 0 */
   }
